73218de5069b04151b777aa03f80806d5fdc58f7
김주영 <jk042386@gmail.com>
2023-01-24 22:09:00 +0900

    Future episode, webtoon at detail screen

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
 import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
@@ -36,6 +37,16 @@ class ApiService {
         episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
       }
       return episodesInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
     }
     throw Error();
   }
